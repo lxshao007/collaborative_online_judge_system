28bf5a17ff8d7f91abbd902aea53a339b5d13466
Lingxiao Shao <lingxiaoahao@gmail.com>
2018-03-29 09:05:41 -0700

    add routefile and new problem detail component. from problem list to problem detail

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { DataService } from './service/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { routing } from './app.routes';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProblemListComponent
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [
     DataService
