aaeed205856d16b2cfc0c990d4a78edf263b1490
Lingxiao Shao <lingxiaoahao@gmail.com>
2018-03-29 09:19:15 -0700

    implemented html&ts file in problem-detail component to display the problem-detail

--- oj-client/src/app/components/problem-detail/problem-detail.component.ts
+++ oj-client/src/app/components/problem-detail/problem-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Problem } from '../../models/problem.model';
+import { DataService } from '../../service/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-problem-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./problem-detail.component.css']
 })
 export class ProblemDetailComponent implements OnInit {
+  problem: Problem;
 
-  constructor() { }
+  constructor(private dataService: DataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params =>{
+      this.problem = this.dataService.getProblem(+params['id']);
+    })
   }
 
 }
